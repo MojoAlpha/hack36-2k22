d96b7a813ddfe6641a6faf57b016423cac5bf077
developer-ishan <ishan@mnnit.ac.in>
2022-04-24 00:47:13 +0530

    Best proxy getting method added

--- App/scripts/run_shell_command.js
+++ App/scripts/run_shell_command.js
@@ -1,6 +1,7 @@
 const sudo = require("sudo-prompt");
+const {exec} = require("child_process")
 
-function run_shell_command(command) {
+function sudo_run_shell_command(command) {
   const options = {
     name: "Electrom",
     icns: "./icn.png",
@@ -13,6 +14,15 @@ function run_shell_command(command) {
   });
 }
 
+function run_shell_command(command) {
+  return new Promise((resolve, reject) => {
+    exec(command, (err, stdout, stderr) => {
+      if (err) return reject(err);
+      return resolve(stdout);
+    });
+  });
+}
+
 //example run
-// run_shell_command("bash wget_test.sh http://edcguest:edcguest@172.31.100.14:3128").then( res => console.log(res) );
-module.exports = { run_shell_command };
+// sudo_run_shell_command("bash wget_test.sh http://edcguest:edcguest@172.31.100.14:3128").then( res => console.log(res) );
+module.exports = { sudo_run_shell_command, run_shell_command };
--- App/scripts/wget_test.sh
+++ App/scripts/wget_test.sh
@@ -1,1 +1,2 @@
-wget https://freetestdata.com/wp-content/uploads/2022/02/Free_Test_Data_15MB_MP4.mp4 -e use_proxy=yes -e https_proxy=$1 2>&1 | grep -o "[0-9.]\+ [KM]*B/s"
+wget https://freetestdata.com/wp-content/uploads/2022/02/Free_Test_Data_1MB_MP4.mp4 -e use_proxy=yes -e https_proxy=$1 2>&1 | grep -o "[0-9.]\+ [KM]*B/s"
+rm -f Free_Test_Data_1MB_MP4.mp4
